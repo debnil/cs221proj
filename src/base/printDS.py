0ba1a76ffbf72ac2c54c315b33173cffe018c780
ezliu <evanliu@stanford.edu>
2015-11-08 23:28:18 -0800

    Created drawing

--- src/base/printDS.py
+++ src/base/printDS.py
@@ -21,3 +21,20 @@ def printGrid(grid):
 				print "	",
 		print
 	return
+
+def printGrid(game):
+    for y in range(game.height):
+        for x in range(game.width):
+            currVertex = game.grid[x][y]
+            vertexChar = "."
+            edgeChar = " "
+            for edge in currVertex.edges:
+                if edge.containsVertex(Vertex(currVertex.x, currVertex.y-1)):
+                    vertexChar = "|"
+                if edge.containsVertex(Vertex(currVertex.x+1, currVertex.y)):
+                    edgeChar = "_"
+            print vertexChar,
+            print edgeChar,
+        print ""
+                                    
+
