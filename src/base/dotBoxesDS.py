6dcece0a1e568506f3bff85b011e3bf83f1b87ed
ezliu <evanliu@stanford.edu>
2015-11-08 17:52:30 -0800

    Fixed edge class

--- src/base/dotBoxesDS.py
+++ src/base/dotBoxesDS.py
@@ -8,5 +8,16 @@ class Vertex:
 
 class Edge:
     def __init__(self, x1, y1, x2, y2):
-        self.src = (x1, y1)
-        self.dest = (x2, y2)
+        self.src = Vertex(x1, y1)
+        self.dest = Vertex(x2, y2)
+
+    # Returns true if the vertex is one of the endpoints of the edge
+    def containsVertex(self, vertex):
+        def equalVertex(first, second):
+            if first.x == second.x and first.y == second.y:
+                return True
+            return False
+
+        if equalVertex(self.src, vertex) or equalVertex(self.dest, vertex):
+            return True
+        return False
